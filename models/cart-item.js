e88bdbfeacde1bb37f9aaf4a9bc5319320151439
Illyas <illyas.deulect@gmail.com>
2021-08-03 23:04:08 +0530

    code cleanup

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,9 +1,9 @@
 class CartItem {
-    constructor(quantity, productPrice, productTitle, sum) {
-      this.quantity = quantity;
-      this.productPrice = productPrice;
-      this.productTitle = productTitle;
-      this.sum = sum;
-    }
+  constructor(quantity, productPrice, productTitle, sum) {
+    this.quantity = quantity;
+    this.productPrice = productPrice;
+    this.productTitle = productTitle;
+    this.sum = sum;
   }
-  export default CartItem;
+}
+export default CartItem;
